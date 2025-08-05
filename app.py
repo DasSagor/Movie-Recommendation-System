4455766966baea13c61d67a0a69069726bf94ff2
DasSagor <dassagor888@gmail.com>
2025-08-06 01:26:19 +0600

    5th August, Finally Done this Movie Recommendation System project

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ import pandas as pd
 import requests
 
 movies_dash = pickle.load(open('movies.pkl','rb'))
-movies_list = movies_dash['title'].values
+movies_list = sorted(movies_dash['title'].values)
 similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
 
 
 def recommend(movie):
@@ -13,14 +23,17 @@ def recommend(movie):
     distances = similarity[movie_index]
     movies = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
 
-    recommend_movies = []
+    recommend_movies_names = []
+    recommend_movies_poster = []
     for i in movies:
-        recommend_movies.append(movies_dash.iloc[i[0]].title)
-    return recommend_movies
+        movie_id = movies_dash.iloc[i[0]].movie_id
+        recommend_movies_poster.append(fetch_poster(movie_id))
+        recommend_movies_names.append(movies_dash.iloc[i[0]].title)
+    return recommend_movies_names,recommend_movies_poster
 
 
-st.title('Movie Recommender System')
 
+st.title('🎬 Movie Recommender System')
 
 selected_movie_name = st.selectbox(
     "Select a movie you like:",
@@ -28,7 +41,12 @@ selected_movie_name = st.selectbox(
 )
 
 
-if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+if st.button('Show Recommendations'):
+    with st.spinner('🔎 Fetching recommendations...'):
+        recommend_movies_names, recommend_movies_poster = recommend(selected_movie_name)
+
+        cols = st.columns(5)
+        for i in range(5):
+            with cols[i]:
+                st.text(recommend_movies_names[i])
+                st.image(recommend_movies_poster[i])
